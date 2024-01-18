8e44785563a0905bbd101e87061cb5e2af60cfbd
Jayasanka <33048395+jayasanka-sack@users.noreply.github.com>
2024-01-18 17:59:31 +0530

    Update E2E test

--- e2e/specs/example.spec.js
+++ e2e/specs/example.spec.js
@@ -1,14 +1,14 @@
-// tests/example.test.js
 import {test, expect} from '@playwright/test';
 test('basic test', async ({ page }) => {
-  await page.goto('https://dev3.openmrs.org/');
+  await page.goto('https://dev3.openmrs.org/openmrs/spa');
 
-  const searchIcon = page.locator('[data-testid=searchPatientIcon]');
-  const searchInput = page.locator('[data-testid=patientSearchBar]');
+  const searchIcon = page.getByTestId('searchPatientIcon')
+  const searchInput = page.getByPlaceholder(/Search for a patient/)
 
-  await searchIcon.click();
-  await searchInput.fill('1000DTV');
+  await searchIcon.click()
+  await searchInput.fill("1000DVT")
 
-  await expect(page.locator("text=1 search result")).toBeVisible();
+  await expect(page.locator('text=1 search result')).toBeVisible()
+  await expect(page.locator('text=Joshua Johnson')).toBeVisible()
 
 });
