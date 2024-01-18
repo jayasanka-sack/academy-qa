152d8b504cec5fe69b5b1ed692659a26ebc987f5
Jayasanka <33048395+jayasanka-sack@users.noreply.github.com>
2024-01-18 16:40:29 +0530

    Completed code

--- e2e/specs/example.spec.js
+++ e2e/specs/example.spec.js
@@ -3,4 +3,12 @@ import {test, expect} from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('https://dev3.openmrs.org/');
 
+  const searchIcon = page.locator('[data-testid=searchPatientIcon]');
+  const searchInput = page.locator('[data-testid=patientSearchBar]');
+
+  await searchIcon.click();
+  await searchInput.fill('1000DTV');
+
+  await expect(page.locator("text=1 search result")).toBeVisible();
+
 });
